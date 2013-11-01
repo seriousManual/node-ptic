ab74ccf5278df4977ff73c263bbb9e4c3d00b934
zaphod1984 <zaphod84@gmx.de>
2013-11-01 15:38:46 +0100

    test added

---
+++ TODO.md
@@ -0,0 +1,4 @@
+# TODO
+
+* test for stop
+* autostart parameter
--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,29 @@
 var expect = require('chai').expect;
+var sinon = require('sinon');
+
+var PTic = require('../');
 
 describe('ptic', function() {
+    var clock;
+
+    before(function() {
+        clock = sinon.useFakeTimers();
+    });
+
+    after(function() {
+        clock.restore();
+    });
 
     it('should tick', function() {
-        expect(true).to.be.true;
+        var ticker = new PTic(100);
+
+        var spy = sinon.spy();
+        ticker.on('tick', spy);
+
+        ticker.start();
+        clock.tick(1000);
+
+        expect(spy.callCount).to.equal(11);
     });
 
 });
