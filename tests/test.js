1fecce9c0e9c121990f784e4883526deef4c1d94
zaphod1984 <zaphod84@gmx.de>
2013-11-01 17:53:41 +0100

    test added

--- tests/test.js
+++ tests/test.js
@@ -47,4 +47,21 @@ describe('ptic', function() {
 
         expect(spy.callCount).to.equal(0);
     });
+
+    it('should stop', function() {
+        var ticker = new PTic(100);
+
+        var spy = sinon.spy();
+        ticker.on('tick', spy);
+
+        ticker.start();
+
+        clock.tick(1000);
+
+        ticker.stop();
+
+        clock.tick(1000);
+
+        expect(spy.callCount).to.equal(11);
+    });
 });
