01dd70fe7ce951e53042068baa05b6ab0f37cd2c
zaphod1984 <zaphod84@gmx.de>
2013-11-15 09:36:52 +0100

    improved ticker example, added comparism with setInterval

--- example/ticker.js
+++ example/ticker.js
@@ -1,11 +1,57 @@
+var util = require('util');
+
 var PTic = require('../');
 
-var ticker = new PTic(100, true);
+function runViaInterval(i, duration, callback) {
+    var o = outputter();
+    var handle = setInterval(o, i);
 
-ticker.on('tick', function() {
-    console.log((new Date()).getTime());
+    setTimeout(function() {
+        console.log(util.format('finished interval with %d calls', o.stats.c));
+
+        clearInterval(handle);
+        callback();
+    }, duration);
+}
+
+function runViaPTic(i, duration, callback) {
+    var ticker = new PTic(i, true);
+
+    var o = outputter();
+    ticker.on('tick', o);
+
+    setTimeout(function() {
+        console.log(util.format('finished ptic usage with %d calls', o.stats.c));
+
+        ticker.stop();
+        callback();
+    }, duration);
+}
+
+function outputter() {
+    var f = function() {
+        f.stats.c++;
+        var t = new Date();
+        console.log(util.format('%ds %dms', t.getSeconds(), t.getMilliseconds()));
+    };
+
+    f.stats = {
+        c: 0
+    };
+
+    return f;
+}
+
+var i = 200;
+var d = 2000;
+
+console.log('this example shows that the regular approach via setInterval is broken');
+console.log('the interval drifts and events get lost\n');
+
+runViaPTic(i, d, function() {
+    console.log('###############################################');
+
+    runViaInterval(i, d, function() {
+        console.log('done!');
+    });
 });
-
-ticker.start();
-
-setTimeout(ticker.stop.bind(ticker), 2000);
